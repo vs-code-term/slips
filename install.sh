9b3a8f1195036d178ef6aa1d39e18fabd606472d
vs-code-term <mastercode868@gmail.com>
2025-10-10 19:34:06 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # Installer for eduslip-ok
 
 # Step 1: Download the main script from your GitHub
-sudo curl -L -o /usr/local/bin/eduslip-ok https://raw.githubusercontent.com/gc-dev-term/slips/main/eduslip-ok.sh
+sudo curl -L -o /usr/local/bin/eduslip-ok https://raw.githubusercontent.com/vs-code-term/slips/main/eduslip-ok.sh
 
 # Step 2: Make it executable
 sudo chmod +x /usr/local/bin/eduslip-ok
